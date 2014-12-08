c1464d1b6ff71bb8232f98d03c0ad631b640d11e
Truls Edvard Stokke <trulses@gmail.com>
2014-12-08 09:04:42 +0100

    Use less memory in align_malloc, elaborate in comments and header file.

--- src/align_malloc.c
+++ src/align_malloc.c
@@ -10,28 +10,33 @@ void * align_malloc (size_t alignment, size_t size) {
   void * p;
   uintptr_t n, r;
 
-  if (alignment > ALIGN_AREA) {
+  if (alignment == 0 || alignment > ALIGN_AREA) {
     errno = EINVAL;
     return NULL;
   }
 
-  p = malloc(size + ALIGN_AREA + alignment + sizeof(void *));
+  p = malloc(size + ALIGN_AREA + sizeof(void *));
 
   if (p == NULL)
     return NULL;
 
+  /* calculate offset from required alignment */
   n = (uintptr_t) p;
   r = alignment - (n % alignment);
 
+  /* we store the malloc'd pointer right before the
+     returned pointer so that we can free later.
+     in case we don't have room to store the pointer
+     make some. */
   if (r < sizeof(void *))
     r += alignment;
 
-  *((void **) (n + r - sizeof(void *))) = p;
+  *((void **) (n + r) - 1) = p;
 
   return (void *) (n + r);
 }
 
 void align_free (void * ptr) {
-  void * p = *((void **) (((char *) ptr) - sizeof(void *)));
+  void * p = *((void **) ptr - 1);
   free(p);
 }
--- src/align_malloc.h
+++ src/align_malloc.h
@@ -3,8 +3,11 @@
 
 #include <stddef.h>
 
+/* returns allocated memory capable of holding size bytes
+   aligned on an alignment sized boundary in bytes. */
 extern void * align_malloc (size_t alignment, size_t size);
 
+/* frees memory previously returned by align_malloc */
 extern void align_free (void * ptr);
 
 #endif /* ALIGN_MALLOC_H */
